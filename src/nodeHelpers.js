bc1d2a977ef72ff235b791fc11b811e27efee4c1
Thomas-C <thomas@flexio.fr>
2018-02-08 17:36:17 +0100

    refactore remove children

--- src/nodeHelpers.js
+++ src/nodeHelpers.js
@@ -1,5 +1,25 @@
-export const removeChildren = (node) => {
-  while (node.firstChild) {
-    node.removeChild(node.firstChild)
+import {
+  should
+} from './should'
+import {
+  isNode
+} from './is'
+
+export const removeChildren = (node, start, end) => {
+  should(isNode(node),
+    'removeChildren: `node` argument should be Node')
+  start = start || 0
+  end = end || node.childNodes.length
+
+  if (!node.hasChildNodes()) {
+    return false
+  }
+
+  should(!!(start <= end),
+    'removeChildren: `start` should be less than `end`')
+
+  while (start < end) {
+    node.removeChild(node.childNodes[start])
+    end--
   }
 }
