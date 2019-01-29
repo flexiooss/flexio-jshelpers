675f4ad40028ca55673714ce9b451b039f18d643
nicolas@flexio.fr <nicolas@flexio.fr>
2019-01-29 17:33:47 +0100

    Create root object

--- src/import.js
+++ src/import.js
@@ -1,1 +1,4 @@
-export const FLEXIO_IMPORT_OBJECT = Symbol('FLEXIO_IMPORT_OBJECT');
+export const FLEXIO_IMPORT_OBJECT = Symbol( 'FLEXIO_IMPORT_OBJECT' );
+if( !window.FLEXIO_IMPORT_OBJECT ){
+    window.FLEXIO_IMPORT_OBJECT = {}
+}
