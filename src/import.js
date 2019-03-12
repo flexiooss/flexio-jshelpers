4103adf23a2262e4ea6613682bbf4c26b912de56
tomacht <thomas@flexio.fr>
2019-03-12 15:23:01 +0100

    fix import symbol

--- src/import.js
+++ src/import.js
@@ -1,8 +1,6 @@
 /* global window */
 export const FLEXIO_IMPORT_OBJECT = Symbol('FLEXIO_IMPORT_OBJECT')
-if (!window) {
-  var window = {}
-}
-if (!window.FLEXIO_IMPORT_OBJECT) {
-  window.FLEXIO_IMPORT_OBJECT = {}
+
+if (!window[FLEXIO_IMPORT_OBJECT]) {
+  window[FLEXIO_IMPORT_OBJECT] = {}
 }
