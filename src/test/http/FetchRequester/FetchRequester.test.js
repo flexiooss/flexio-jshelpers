226f4566d714729d8d6a64e733c93327d3ded190
tomacht <thomas@flexio.fr>
2019-04-01 11:55:46 +0200

    start test for requester

--- src/test/http/FetchRequester/FetchRequester.test.js
+++ src/test/http/FetchRequester/FetchRequester.test.js
@@ -1,10 +1,7 @@
 /* global runTest */
 import {FetchRequester} from '../../../js/http/FetchRequester/FetchRequester'
 import {TestCase} from 'code-altimeter-js'
-const fetch = require('node-fetch');
-global.Headers = fetch.Headers;
 const assert = require('assert')
-global.Headers = global.Headers || require("fetch-headers");
 
 export class TestFetchRequester extends TestCase {
   testPath() {
