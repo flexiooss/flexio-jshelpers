c825749f80c2ebbe97ef0a157cd624d353fbf1f2
Basheitan <antoine@flexio.fr>
2019-04-17 09:24:12 +0200

    add globalScope for wokers prankers

--- src/js/import.js
+++ src/js/import.js
@@ -1,6 +1,15 @@
 /* global window */
 export const FLEXIO_IMPORT_OBJECT = Symbol('FLEXIO_IMPORT_OBJECT')
 
-if (!window[FLEXIO_IMPORT_OBJECT]) {
+const getGlobal = function () {
+  if (typeof self !== 'undefined') { return self; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+}
+
+export const globalScope = getGlobal()
+
+if (!globalScope[FLEXIO_IMPORT_OBJECT]) {
   window[FLEXIO_IMPORT_OBJECT] = {}
 }
