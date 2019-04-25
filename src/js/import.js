5cc3d424b145a6e549e648163a03a527b15b745e
tomacht <thomas@flexio.fr>
2019-04-25 08:04:41 +0200

    globalFlexioImport

--- src/js/import.js
+++ src/js/import.js
@@ -1,11 +1,17 @@
-/* global window */
+/* global window,self,global */
 export const FLEXIO_IMPORT_OBJECT = Symbol('FLEXIO_IMPORT_OBJECT')
 
-const getGlobal = function () {
-  if (typeof self !== 'undefined') { return self; }
-  if (typeof window !== 'undefined') { return window; }
-  if (typeof global !== 'undefined') { return global; }
-  throw new Error('unable to locate global object');
+const getGlobal = function() {
+  if (typeof self !== 'undefined') {
+    return self
+  }
+  if (typeof window !== 'undefined') {
+    return window
+  }
+  if (typeof global !== 'undefined') {
+    return global
+  }
+  throw new Error('unable to locate global object')
 }
 
 export const globalScope = getGlobal()
@@ -13,3 +19,4 @@ export const globalScope = getGlobal()
 if (!globalScope[FLEXIO_IMPORT_OBJECT]) {
   window[FLEXIO_IMPORT_OBJECT] = {}
 }
+export const globalFlexioImport = globalScope[FLEXIO_IMPORT_OBJECT]
